bf56c863c074e5a3b4bbb57f1b149417ec23da5b
Sanjay Kumar <sanjaykumar200599@gmail.com>
2026-01-21 18:03:54 +0530

    Added the entities

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,10 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookingService {
+    private User user;
+    public UserBookingService(User user1){
+        this.user=user1;
+    }
+}
