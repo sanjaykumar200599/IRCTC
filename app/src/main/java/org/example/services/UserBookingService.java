6d37325d05260487cba3676c525a891f33f0c8d4
Sanjay Kumar <sanjaykumar200599@gmail.com>
2026-01-22 16:12:24 +0530

    updated

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -2,9 +2,16 @@ package org.example.services;
 
 import org.example.entities.User;
 
-public class UserBookingService {
+import java.io.File;
+
+public class UserBookingService
+{
     private User user;
+
+    private static final String USERS_PATH="../localDb/user.json";
+
     public UserBookingService(User user1){
         this.user=user1;
+        File users =new File();
     }
 }
